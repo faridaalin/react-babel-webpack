8b9496f1483f7b13b8aa4d2b0dedec65fc44eb46
Farida Alin <farida@Faridas-MacBook-Air.local>
2021-02-03 12:41:02 +0100

    init

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+// import moment from 'moment'
+import './sass/style.scss'
+// const today = moment().format('MMMM Do YYYY, h:mm:ss a')
+
+// console.log(today)
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+// const webpack = require('webpack')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const path = require('path')
+
+// module.exports = {
+//   entry: path.resolve(__dirname, 'src', 'index.tsx'),
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   plugins: [
+//     new webpack.HotModuleReplacementPlugin(),
+//     new HtmlWebpackPlugin({
+//       tempalte: './src/index.html',
+//       filename: './index.html',
+//     }),
+//   ],
+
+//   devServer: {
+//     contentBase: path.resolve(__dirname, 'dist'),
+//     hot: true,
+//     port: 9000,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.s[ac]ss$/i,
+//         use: [
+//           'style-loader', // Creates `style` nodes from JS strings
+//           'css-loader', // Translates CSS into CommonJS
+//           'sass-loader', // Compiles Sass to CSS
+//         ],
+//       },
+//       {
+//         test: /\.tsx?$/,
+//         use: 'babel-loader',
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   resolve: {
+//     extensions: ['.tsx', '.jsx', '.js', '.ts'],
+//   },
+// }
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader', // Creates `style` nodes from JS strings
+          'css-loader', // Translates CSS into CommonJS
+          'sass-loader', // Compiles Sass to CSS
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.jsx', '.js', '.ts'],
+  },
+}
